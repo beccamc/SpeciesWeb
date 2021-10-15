4bd98341bc54d601aecd1b73cae4d97ee9a3c55d
Becca McHenry <mchenry.rebecca@gmail.com>
2021-10-15 09:52:45 -0500

    PR cleanup, remove unecessary files

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
-using SpeciesWeb.Areas.Identity;
 using SpeciesWeb.Data;
 
 namespace SpeciesWeb
